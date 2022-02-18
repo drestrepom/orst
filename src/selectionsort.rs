e3a5bf44c50a76dccd13acf999327b74c73d9c5a
Diego Restrepo <drestrepo@fluidattacks.com>
2022-02-18 10:14:53 -0500

    feat(back): add selection sort

---
+++ src/selectionsort.rs
@@ -0,0 +1,23 @@
+use super::Sorter;
+
+pub struct SelectionSort;
+
+impl Sorter for SelectionSort {
+    fn sort<T>(&self, slice: &mut [T])
+    where
+        T: Ord,
+    {
+        for unsorted in 0..slice.len() {
+            let small_in_rest = slice[unsorted..]
+                .iter()
+                .enumerate()
+                .min_by_key(|&(_, v)| v)
+                .map(|(i, _)| unsorted + i)
+                .expect("slice is non-empty");
+
+            if unsorted != small_in_rest {
+                slice.swap(unsorted, small_in_rest)
+            }
+        }
+    }
+}

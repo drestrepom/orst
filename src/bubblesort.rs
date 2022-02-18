04393dc193ad5ed37eb068cc32379d5be16c462d
Diego Restrepo <drestrepo@fluidattacks.com>
2022-02-18 10:13:55 -0500

    feat(back): add bubblesort

---
+++ src/bubblesort.rs
@@ -0,0 +1,20 @@
+use super::Sorter;
+
+pub struct BubbleSort;
+impl Sorter for BubbleSort {
+    fn sort<T>(&self, slice: &mut [T])
+    where
+        T: Ord,
+    {
+        let mut swapped = true;
+        while swapped {
+            swapped = false;
+            for i in 1..slice.len() {
+                if slice[i - 1] > slice[i] {
+                    slice.swap(i - 1, i);
+                    swapped = true;
+                }
+            }
+        }
+    }
+}
